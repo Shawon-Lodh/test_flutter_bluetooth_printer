cfe61a274a1e02943685a1671d4fa13e7b5db037
Shawon-Lodh <50014582+Shawon-Lodh@users.noreply.github.com>
2022-02-07 18:00:01 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/test_flutter_bluetooth_printer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_flutter_bluetooth_printer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,260 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:intl/intl.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:oktoast/oktoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OKToast(
+      child: MaterialApp(
+        title: 'Bluetooth demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Bluetooth demo'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  PrinterBluetoothManager printerManager = PrinterBluetoothManager();
+  List<PrinterBluetooth> _devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    printerManager.scanResults.listen((devices) async {
+      // print('UI: Devices found ${devices.length}');
+      setState(() {
+        _devices = devices;
+      });
+    });
+  }
+
+  void _startScanDevices() {
+    setState(() {
+      _devices = [];
+    });
+    printerManager.startScan(Duration(seconds: 4));
+  }
+
+  void _stopScanDevices() {
+    printerManager.stopScan();
+  }
+
+  Future<List<int>> demoReceipt(
+      PaperSize paper, CapabilityProfile profile) async {
+    final Generator ticket = Generator(paper, profile);
+    List<int> bytes = [];
+
+    // Print image
+    // final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
+    // final Uint8List imageBytes = data.buffer.asUint8List();
+    // final Image? image = decodeImage(imageBytes);
+    // bytes += ticket.image(image);
+
+    bytes += ticket.text('GLOBAL LEAF TOBACCO COMPANY LTD',
+        styles: PosStyles(align: PosAlign.center));
+
+    bytes += ticket.text('Crop Year 2021',
+        styles: PosStyles(align: PosAlign.center));
+
+    bytes += ticket.text('Tobacco Transport Permit',
+        styles: PosStyles(
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ),
+        linesAfter: 1);
+
+    bytes += ticket.qrcode("testtesttesttesttesttesttesttest",size: QRSize.Size8,align: PosAlign.center);
+
+    bytes += ticket.text('Name : Ramiz Uddin',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Father Name : Sukkor Molla',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Reg No : 1234567890',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Bale : 5',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Ext Center : Rongpur',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.text('Tp Issue Date : ${DateTime.now()}',
+        styles: PosStyles(align: PosAlign.center));
+    bytes += ticket.hr();
+
+    bytes += ticket.text('রমিজ উদ্দিন',
+        styles: PosStyles(align: PosAlign.center));
+
+    // //Print QR Code from image
+    // try {
+    //   const String qrData = 'example.com';
+    //   const double qrSize = 200;
+    //   final uiImg = await QrPainter(
+    //     data: qrData,
+    //     version: QrVersions.auto,
+    //     gapless: false,
+    //   ).toImageData(qrSize);
+    //   final dir = await getTemporaryDirectory();
+    //   final pathName = '${dir.path}/qr_tmp.png';
+    //   final qrFile = File(pathName);
+    //   final imgFile = await qrFile.writeAsBytes(uiImg!.buffer.asUint8List());
+    //   final img = decodeImage(imgFile.readAsBytesSync());
+    //
+    //   bytes += ticket.image(img!);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    // Print QR Code using native function
+    // bytes += ticket.qrcode('example.com');
+
+    bytes += ticket.feed(2);
+    bytes += ticket.text('Thank you!',
+        styles: PosStyles(align: PosAlign.center, bold: true));
+
+    final now = DateTime.now();
+    final formatter = DateFormat('MM/dd/yyyy H:m');
+    final String timestamp = formatter.format(now);
+    bytes += ticket.text(timestamp,
+        styles: PosStyles(align: PosAlign.center), linesAfter: 2);
+
+    // Print QR Code from image
+    // try {
+    //   const String qrData = 'example.com';
+    //   const double qrSize = 200;
+    //   final uiImg = await QrPainter(
+    //     data: qrData,
+    //     version: QrVersions.auto,
+    //     gapless: false,
+    //   ).toImageData(qrSize);
+    //   final dir = await getTemporaryDirectory();
+    //   final pathName = '${dir.path}/qr_tmp.png';
+    //   final qrFile = File(pathName);
+    //   final imgFile = await qrFile.writeAsBytes(uiImg.buffer.asUint8List());
+    //   final img = decodeImage(imgFile.readAsBytesSync());
+
+    //   bytes += ticket.image(img);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    // Print QR Code using native function
+    // bytes += ticket.qrcode('example.com');
+
+    //Print image
+    // try {
+    //   final image = decodeImage(File('assets/images/default.png').readAsBytesSync())!;
+    //   bytes += ticket.image(image);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    ticket.feed(2);
+    ticket.cut();
+    return bytes;
+  }
+
+  void _testPrint(PrinterBluetooth printer) async {
+    printerManager.selectPrinter(printer);
+
+    // TODO Don't forget to choose printer's paper
+    const PaperSize paper = PaperSize.mm80;
+    final profile = await CapabilityProfile.load();
+
+    // TEST PRINT
+    // final PosPrintResult res =
+    // await printerManager.printTicket(await testTicket(paper));
+
+    // DEMO RECEIPT
+    final PosPrintResult res =
+    await printerManager.printTicket((await demoReceipt(paper, profile)));
+
+    showToast(res.msg);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: _devices.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(
+              onTap: () => _testPrint(_devices[index]),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 60,
+                    padding: EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.print),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(_devices[index].name ?? ''),
+                              Text(_devices[index].address!),
+                              Text(
+                                'Click to print a test receipt',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const Divider(),
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: printerManager.isScanningStream,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: _stopScanDevices,
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(
+              child: Icon(Icons.search),
+              onPressed: _startScanDevices,
+            );
+          }
+        },
+      ),
+    );
+  }
+
+}
